660a006905e6154dd6bc269c00a7e4d529590c3c
Majikujanisch <karottennase123@gmail.com>
2020-02-06 12:48:55 +0100

    save meth deleted, ausgabe

--- StaticProject/src/RepositoryItem.java
+++ StaticProject/src/RepositoryItem.java
@@ -6,7 +6,6 @@ public interface RepositoryItems {
     // WICHTIG: Interfaces geben einen Vertrag vor
     // es werden nur die Methodenköpfe angegeben, es exisitiert noch kein Code. Diesen müssen die implementierenden
     // Klassen bereitstellen
-    public boolean Save();
     public boolean Insert(Item item);
     public boolean Remove(int primaryKey);
     public boolean Update(int primaryKey, Item newItemData);
--- StaticProject/src/RepositoryItemsDB.java
+++ StaticProject/src/RepositoryItemsDB.java
@@ -5,38 +5,40 @@ public class RepositoryItemsDB implements RepositoryItems {
     // implementiert eine Klasse ein Interface, so verpflichtet sie sich dazu, dass sie alle Methoden ausprogrammiert
     // WICHTIG: es können beliebig viele weitere Members (Felder, Methoden) vorhanden sein
 
-    @Override
-    public boolean Save() {
-        return false;
-    }
 
     @Override
     public boolean Insert(Item item) {
+        System.out.println("Artikel wurde in DB Abgespeichert");
         return false;
     }
 
     @Override
     public boolean Remove(int primaryKey) {
+        System.out.println("Artikel wurde aus DB gelöscht");
         return false;
     }
 
     @Override
     public boolean Update(int primaryKey, Item newItemData) {
+        System.out.println("Artikel wurde in DB geändert");
         return false;
     }
 
     @Override
     public List<Item> SearchByName(String name) {
+        System.out.println("Artikelliste aus DB - NAME");
         return null;
     }
 
     @Override
     public List<Item> SearchByCategory(String category) {
+        System.out.println("Artikelliste aus DB - Kategorie");
         return null;
     }
 
     @Override
     public Item SearchByPrimaryKey(int primaryKey) {
+        System.out.println("Artikelliste aus DB - PK");
         return null;
     }
 }
